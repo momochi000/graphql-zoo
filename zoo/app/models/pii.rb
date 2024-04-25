f0adf64008506b0d99ce633bd7c90fca22f90954
Zachery Chin <zchin@sproutpost.net>
2024-04-25 12:10:01 -0700

    encrypt pii fields

--- zoo/app/models/pii.rb
+++ zoo/app/models/pii.rb
@@ -1,3 +1,9 @@
 class Pii < ApplicationRecord
   belongs_to :employee
+
+  # Note need to add deterministic: true if we need to query by these fields
+  encrypts :first_name
+  encrypts :last_name
+  encrypts :email
+  encrypts :phone
 end
