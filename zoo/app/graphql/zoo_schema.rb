25d77042119e2ff2a7d8966bdf1c1619b7341e6f
Zachery Chin <zchin@sproutpost.net>
2024-04-26 14:58:26 -0700

    rails g graphql:install

---
+++ zoo/app/graphql/zoo_schema.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module GraphQL
+  class ZooSchema < GraphQL::Schema
+  end
+end
--- zoo/config/routes.rb
+++ zoo/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post "/graphql",          to: "graphql/base#execute"
+  get  "/graphiql",         to: "graphql/base#graphiql"
+  get  "/graphql/describe", to: "graphql/base#describe"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
