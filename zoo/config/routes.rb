0ffec849438ea2c6cdbf94180e6e4592b107e922
Zachery Chin <zchin@sproutpost.net>
2024-04-26 15:28:18 -0700

    setup graphiql for development

--- zoo/config/routes.rb
+++ zoo/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   post "/graphql",          to: "graphql#execute"
-  get  "/graphiql",         to: "graphql/base#graphiql"
+  get  "/graphiql",         to: "graphql#graphiql"
   get  "/graphql/describe", to: "graphql#describe"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
