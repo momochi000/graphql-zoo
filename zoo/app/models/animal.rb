309d136975518943f9a25351ed254b1941161fca
Zachery Chin <zchin@sproutpost.net>
2024-04-29 16:30:13 -0700

    expose dietary requirements in animal

--- zoo/app/models/animal.rb
+++ zoo/app/models/animal.rb
@@ -1,5 +1,9 @@
 class Animal < ApplicationRecord
   belongs_to :habitat
 
-  enum status: [:healthy, :sick, :injured, :depressed]
+  enum status: [:healthy, :sick, :injured, :depressed, :needs_attention]
+
+  def dietary_requirements
+    info["dietary_requirements"]
+  end
 end
